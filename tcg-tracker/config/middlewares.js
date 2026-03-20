76784b3761fd29c2544d5f74b3062e2271ec35d9
Cecilie <ceciliehansen402@gmail.com>
2026-03-20 02:12:49 +0100

    Update middlewares.js

--- tcg-tracker/config/middlewares.js
+++ tcg-tracker/config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:5500',
+        'https://tcg-tracker-1997.web.app',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
